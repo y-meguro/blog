97fd1dce447e1555640f8b4a0029e8bd040c45cc
Yohei <y.meguro.2008@gmail.com>
2024-09-19 14:07:39 +0900

    fix

--- components/markdown/LinkCard/index.tsx
+++ components/markdown/LinkCard/index.tsx
@@ -3,59 +3,64 @@ import { load } from 'cheerio'
 import styles from './styles.module.css'
 
 export async function LinkCard({ url }: { url: string }) {
-  const res = await axios.get(url)
-  const _$ = load(res.data)
-  const title =
-    _$("meta[property='og:title']").attr('content') || _$('title').text()
-  const description = _$("meta[property='og:description']").attr('content')
-
-  let imageSrc = _$("meta[property='og:image']").attr('content') ?? ''
   try {
-    const { status } = await axios.get(imageSrc)
-    imageSrc = status < 400 ? imageSrc : ''
-  } catch (err) {
-    imageSrc = ''
-  }
+    const res = await axios.get(url)
+    const _$ = load(res.data)
+    const title =
+      _$("meta[property='og:title']").attr('content') || _$('title').text()
+    const description = _$("meta[property='og:description']").attr('content')
 
-  let icon =
-    _$('link[rel="shortcut icon"]').attr('href') ||
-    _$('link[rel="icon"]').attr('href') ||
-    ''
-  if (icon && !icon.startsWith('http')) {
-    if (icon.startsWith('/')) {
-      icon = new URL(url).origin + icon
-    } else {
-      icon = url + icon
+    let imageSrc = _$("meta[property='og:image']").attr('content') ?? ''
+    try {
+      const { status } = await axios.get(imageSrc)
+      imageSrc = status < 400 ? imageSrc : ''
+    } catch (err) {
+      imageSrc = ''
     }
-  }
-  try {
-    const { status } = await axios.get(icon)
-    icon = status < 400 ? icon : ''
-  } catch (err) {
-    icon = ''
-  }
 
-  const hostname = new URL(url).hostname
+    let icon =
+      _$('link[rel="shortcut icon"]').attr('href') ||
+      _$('link[rel="icon"]').attr('href') ||
+      ''
+    if (icon && !icon.startsWith('http')) {
+      if (icon.startsWith('/')) {
+        icon = new URL(url).origin + icon
+      } else {
+        icon = url + icon
+      }
+    }
+    try {
+      const { status } = await axios.get(icon)
+      icon = status < 400 ? icon : ''
+    } catch (err) {
+      icon = ''
+    }
 
-  return (
-    <div className={styles.Card_Container}>
-      <a href={url} target="_brank">
-        <div className={styles.Card_Main}>
-          {title && <div className={styles.Card_Title}>{title}</div>}
-          {description && (
-            <div className={styles.Card_Description}>{description}</div>
+    const hostname = new URL(url).hostname
+
+    return (
+      <div className={styles.Card_Container}>
+        <a href={url} target="_brank">
+          <div className={styles.Card_Main}>
+            {title && <div className={styles.Card_Title}>{title}</div>}
+            {description && (
+              <div className={styles.Card_Description}>{description}</div>
+            )}
+            <div className={styles.LinkCard_Url}>
+              {icon && <img src={icon} width="16" height="16" alt="" />}
+              <div>{hostname}</div>
+            </div>
+          </div>
+          {imageSrc && (
+            <div className={styles.LinkCard_Thumbnail}>
+              <img src={imageSrc} alt={title} />
+            </div>
           )}
-          <div className={styles.LinkCard_Url}>
-            {icon && <img src={icon} width="16" height="16" alt="" />}
-            <div>{hostname}</div>
-          </div>
-        </div>
-        {imageSrc && (
-          <div className={styles.LinkCard_Thumbnail}>
-            <img src={imageSrc} alt={title} />
-          </div>
-        )}
-      </a>
-    </div>
-  )
+        </a>
+      </div>
+    )
+  } catch (err) {
+    console.log(url, err)
+    return null
+  }
 }
